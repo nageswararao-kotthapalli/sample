e14581e45f7a0d0130ce8ed4752726ef400d9f23
unknown <tmsraji99@gmail.com>
2019-12-27 18:31:01 +0530

    dnsjkfsdj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sample';
+  title = 'https://reqres.in/api/users';
+  Modeldata: string;
+  display: boolean;
+  data;
+  constructor(private http:HttpClient){
+    this.getDetails();
+   const refresh = setInterval(()=>{
+    this.getDetails();
+   },10000);
+  }
+  getDetails(){
+      this.http.get(this.title).subscribe(
+        res => { //qres.in/api/users Success
+           this.data = res['data']
+        console.log(res['data']);
+        }
+      );
+  }
+  RowData(row) {
+    this.Modeldata = JSON.stringify(row);
+    this.display = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{ FormsModule}from '@angular/forms'
 import { AppComponent } from './app.component';
 
+import{HttpClientModule} from '@angular/common/http'
+import {TableModule} from 'primeng/table';
+import {DialogModule} from 'primeng/dialog';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule, TableModule,
+    DialogModule,
+    BrowserAnimationsModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
